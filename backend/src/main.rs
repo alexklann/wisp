671d38f0fba9fed1666ec28d0d49209b049ac74c
klann <alex@klann.me>
2026-03-14 15:00:46 +0100

    init: create project

---
+++ backend/src/main.rs
@@ -0,0 +1,14 @@
+use axum::{Router, routing::get};
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
